cc20eae17ee78d64e14c0b1d9f595f0ac77a5a42
Anderson Cusma <andersoncusma20@gmail.com>
2018-10-07 01:23:26 -0500

    Usuario y token listos

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Requires es una importacion de terceros o personalizadas para que funcione algo
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true }, (err, res) => {
@@ -13,13 +24,11 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', { useNewUrlP
 });
 
 
+
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 // Escuchar peticiones
 app.listen(3000, () => {
---
+++ config/config.js
@@ -0,0 +1,1 @@
+module.exports.SEED = '^desarrollo$agitado@netflix';
